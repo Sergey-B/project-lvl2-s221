46f13e79942ef402483c96e0d9787c625b460ef7
Sergey Belov <belovtorzhok@gmail.com>
2018-02-21 22:11:54 +0300

    add comparison for ini files

--- __tests__/diff.test.js
+++ __tests__/diff.test.js
@@ -1,24 +1,18 @@
 import path from 'path';
 import fs from 'fs';
-import yaml from 'js-yaml';
 import gendiff from '../src';
 
-const readFixturesFile = fileName => fs.readFileSync(path.join(__dirname, `./__fixtures__/${fileName}`), 'utf8');
+const buildFilePath = fileName => path.join(__dirname, `./__fixtures__/${fileName}`);
+const expectedResult = fs.readFileSync(buildFilePath('result.txt'), 'utf8');
 
 test('diff flat json', () => {
-  const firstConfigObject = JSON.parse(readFixturesFile('before.json'));
-  const secondConfigObject = JSON.parse(readFixturesFile('after.json'));
-
-  const expectedDiff = readFixturesFile('result.txt');
-
-  expect(gendiff(firstConfigObject, secondConfigObject)).toEqual(expectedDiff);
+  expect(gendiff(buildFilePath('before.json'), buildFilePath('after.json'))).toEqual(expectedResult);
 });
 
 test('diff flat yaml', () => {
-  const firstConfigObject = yaml.safeLoad(readFixturesFile('before.yaml'));
-  const secondConfigObject = yaml.safeLoad(readFixturesFile('after.yaml'));
+  expect(gendiff(buildFilePath('before.yaml'), buildFilePath('after.yaml'))).toEqual(expectedResult);
+});
 
-  const expectedDiff = readFixturesFile('result.txt');
-
-  expect(gendiff(firstConfigObject, secondConfigObject)).toEqual(expectedDiff);
+test('diff flat ini', () => {
+  expect(gendiff(buildFilePath('before.ini'), buildFilePath('after.ini'))).toEqual(expectedResult);
 });
